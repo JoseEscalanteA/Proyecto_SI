db84b29da5743b88f33e167ec0412828d4244dde
CesarJ01 <cesar.jimenez.mayorga@alumnos.uta.cl>
2025-08-07 15:50:07 -0400

    Insertar cors para conexión

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Configuración CORS 
+  app.enableCors({
+    origin: true, // Permite todos los orígenes (en producción especifica dominios)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Si necesitas enviar cookies/tokens
+    allowedHeaders: 'Content-Type,Authorization,X-Requested-With,Accept',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
